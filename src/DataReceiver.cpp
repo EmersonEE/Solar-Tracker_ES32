ba043cddbd47101ca941c8e508a20fcad6ee4405
EmersonEE <perezemerson85@gmail.com>
2025-04-04 07:26:16 -0600

    Mofificando Librerias

--- src/DataReceiver.cpp
+++ src/DataReceiver.cpp
@@ -1,19 +1,17 @@
 // datareceiver.cpp
 #include "DataReceiver.h"
 
-DataReceiver::DataReceiver(LDR &ldr1, LDR &ldr2, LDR &ldr3, LDR &ldr4, LDR &ldr5, LDR &ldr6)
+DataReceiver::DataReceiver(LDR &ldr1, LDR &ldr2, LDR &ldr3, LDR &ldr4)
 {
-    ldrs[0] = &ldr1;
-    ldrs[1] = &ldr2;
-    ldrs[2] = &ldr3;
-    ldrs[3] = &ldr4;
-    ldrs[4] = &ldr5;
-    ldrs[5] = &ldr6;
+    ldrs[0] = &ldr1; // Superior Derecha
+    ldrs[1] = &ldr2; // Inferior Derecha
+    ldrs[2] = &ldr3; // Superior Izquierda
+    ldrs[3] = &ldr4; // Inferior Izquierda
 }
 
 void DataReceiver::update()
 {
-    for (uint8_t i = 0; i < 6; i++)
+    for (uint8_t i = 0; i < 4; i++)
     {
         rawValues[i] = ldrs[i]->readRaw(); // Actualiza lecturas
     }
@@ -24,7 +22,7 @@ uint8_t DataReceiver::getBrightestLDR()
     uint8_t brightestIdx = 0;
     uint16_t maxValue = rawValues[0];
 
-    for (uint8_t i = 1; i < 6; i++)
+    for (uint8_t i = 1; i < 4; i++)
     {
         if (rawValues[i] > maxValue)
         {
@@ -38,45 +36,36 @@ uint8_t DataReceiver::getBrightestLDR()
 
 float DataReceiver::getLDRVoltage(uint8_t ldrNum)
 {
-    if (ldrNum < 1 || ldrNum > 6)
+    if (ldrNum < 1 || ldrNum > 4)
         return 0.0; // Validación
     return ldrs[ldrNum - 1]->readVoltage();
 }
 
 float DataReceiver::getLDRAverageUp()
 {
-    float sum = 0.0f;
-    for (uint8_t i = 0; i < 3; i++)
-    {
-        sum += ldrs[i]->readVoltage();
-    }
-    return sum / 3.0f;
+    float sum = ldrs[0]->readVoltage() + ldrs[2]->readVoltage();
+    return sum / 2.0f;
 }
 
 float DataReceiver::getLDRAverageDown()
 {
-    float sum = 0.0f;
-    for (uint8_t i = 3; i < 6; i++)
-    {
-        sum += ldrs[i]->readVoltage();
-    }
-    return sum / 3.0f;
+    float sum = ldrs[1]->readVoltage() + ldrs[3]->readVoltage();
+    return sum / 2.0f;
 }
 
 float DataReceiver::getLDRAverageLeft()
 {
-    float sum = ldrs[0]->readVoltage() + ldrs[3]->readVoltage();
+    float sum = ldrs[0]->readVoltage() + ldrs[1]->readVoltage();
     return sum / 2.0f;
 }
 
 float DataReceiver::getLDRAverageRight()
 {
-    float sum = ldrs[2]->readVoltage() + ldrs[5]->readVoltage();
+    float sum = ldrs[2]->readVoltage() + ldrs[3]->readVoltage();
     return sum / 2.0f;
 }
 
-float DataReceiver::getLDRAverageCenter()
-{
-    float sum = ldrs[1]->readVoltage() + ldrs[4]->readVoltage();
-    return sum / 2.0f;
-}
+// #define LDR1_PIN 34 // Superior Izquierda 0
+// #define LDR2_PIN 35 // Inferior Izquierda 1
+// #define LDR3_PIN 32 // Superior Derecha 2
+// #define LDR4_PIN 33 // Inferior Derecha 3
